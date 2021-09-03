e6fae600259466c9e9a09b2b0231a57fd366c4e0
John Scott <john@jjrscott.com>
2021-09-03 22:04:18 +0100

    Initial Commit

---
+++ XCTestBackChannel/XCTestBackChannel.h
@@ -0,0 +1,18 @@
+//
+//  XCTestBackChannel.h
+//  XCTestBackChannel
+//
+//  Created by John Scott on 9/3/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XCTestBackChannel.
+FOUNDATION_EXPORT double XCTestBackChannelVersionNumber;
+
+//! Project version string for XCTestBackChannel.
+FOUNDATION_EXPORT const unsigned char XCTestBackChannelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XCTestBackChannel/PublicHeader.h>
+
+
---
+++ XCTestBackChannelDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XCTestBackChannelDemo
+//
+//  Created by John Scott on 9/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
